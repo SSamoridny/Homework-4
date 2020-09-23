0701c743cb5291c95237c2257860d221f5516af9
Shaun Samoridny <ssamoridny@gmail.com>
2020-09-23 01:15:53 -0600

    Added the ability to click thhrough the quiz

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+var score = 0
+
 var startButton = document.querySelector('.start-btn')
+var answerOne = document.querySelector('#answer-buttons1')
+var answerTwo = document.querySelector('#answer-buttons2')
+var answerThree = document.querySelector('#answer-buttons3')
+var answerFour = document.querySelector('#answer-buttons4')
+var answerFive = document.querySelector('#answer-buttons5')
+var beginH1 = document.querySelector('.begin')
+
+
 var questionContainer1 = document.querySelector('.question1')
 var questionContainer2 = document.querySelector('.question2')
+var questionContainer3 = document.querySelector('.question3')
+var questionContainer4 = document.querySelector('.question4')
+var questionContainer5 = document.querySelector('.question5')
+
 
 //Start Button
 startButton.addEventListener('click', startGame)
@@ -8,6 +22,7 @@ startButton.addEventListener('click', startGame)
 function startGame () {
     console.log('Started')
     startButton.classList.add('hide')
+    beginH1.classList.add('hide')
     questionContainer1.classList.remove('hide')
 }
 
@@ -16,14 +31,31 @@ function startGame () {
 //incorrect score -5 seconds
 
 //pick an answer Q1
+answerOne.addEventListener('click', answer1)
+
 function answer1 () {
-    var answerOne = document.querySelector('.btn1')
     questionContainer1.classList.add('hide')
-    answerOne.addEventListener('click', answer1)
+    questionContainer2.classList.remove('hide')
 }
 
 //move to next question
-function question2 () {
-    questionContainer1.classList.add('hide')
-    questionContainer2.classList.remove('hide')
+answerTwo.addEventListener('click', answer2)
+
+function answer2 () {
+    questionContainer2.classList.add('hide')
+    questionContainer3.classList.remove('hide')
+}
+
+answerThree.addEventListener('click', answer3)
+
+function answer3 () {
+    questionContainer3.classList.add('hide')
+    questionContainer4.classList.remove('hide')
+}
+
+answerFour.addEventListener('click', answer4)
+
+function answer4 () {
+    questionContainer4.classList.add('hide')
+    questionContainer5.classList.remove('hide')
 }
