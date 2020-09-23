e26012fa694595cf71e2d7376eb0b13641a3c4ba
Shaun Samoridny <ssamoridny@gmail.com>
2020-09-22 21:42:33 -0600

    added the questions and the beginnings of getting the program to move from one question to the next

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-//start button
+var startButton = document.querySelector('.start-btn')
+var questionContainer1 = document.querySelector('.question1')
+var questionContainer2 = document.querySelector('.question2')
+
+//Start Button
+startButton.addEventListener('click', startGame)
+
+function startGame () {
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionContainer1.classList.remove('hide')
+}
 
 //countdown timer
 
 //incorrect score -5 seconds
 
-//
+//pick an answer Q1
+function answer1 () {
+    var answerOne = document.querySelector('.btn1')
+    questionContainer1.classList.add('hide')
+    answerOne.addEventListener('click', answer1)
+}
+
+//move to next question
+function question2 () {
+    questionContainer1.classList.add('hide')
+    questionContainer2.classList.remove('hide')
+}
